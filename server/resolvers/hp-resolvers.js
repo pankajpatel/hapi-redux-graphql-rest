fbff802285a7a951af3e2398d37d5c4f8a3b604c
Pankaj Patel <patel.pankaj@live.in>
2017-02-03 23:57:54 +0100

    removed sample data

--- server/resolvers/hp-resolvers.js
+++ server/resolvers/hp-resolvers.js
@@ -1,16 +1,5 @@
 const fetch = require('node-fetch');
 
-const authors = [
-  { id: 1, firstName: 'Tom', lastName: 'Coleman' },
-  { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
-];
-
-const posts = [
-  { id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
-  { id: 2, authorId: 2, title: 'GraphQL Rocks', votes: 3 },
-  { id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
-];
-
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 function fetchResponseByURL(relativeURL) {
